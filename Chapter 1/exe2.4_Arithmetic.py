63d2b405e3e0297c1b24a7fe666d35352512bed0
Jose Valim <joseaugustovalim1994@gmail.com>
2025-12-10 20:41:18 -0300

    feat: Exercise 01

---
+++ Chapter 1/exe2.4_Arithmetic.py
@@ -0,0 +1,13 @@
+sumOperation = -5 + 2
+minusOperation =  0 - 2
+timesOperation = 5 * 2
+divisionOperation = 7.5 / 2
+trueDivisionOperation = 7 // 4
+powOperation =  5 ** 2
+
+print(sumOperation)
+print(minusOperation)
+print(timesOperation)
+print(divisionOperation)
+print(trueDivisionOperation) # Trunca o resultado para o inteiro menor que o resultado da divisao
+print(powOperation)
