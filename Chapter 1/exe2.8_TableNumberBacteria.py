7f4f1af7cf812dfb1c2ada0bb31e10da329e0bd0
Jose Valim <joseaugustovalim1994@gmail.com>
2026-01-13 17:15:59 -0300

    feat: exercise 8 number of bacteria

---
+++ Chapter 1/exe2.8_TableNumberBacteria.py
@@ -0,0 +1,12 @@
+hour0 = 200 * (2 ** 0)
+hour5 = 200 * (2 ** 5)
+hour10 = 200 * (2 ** 10)
+hour15 = 200 * (2 ** 15)
+
+# Data Table
+
+print('Hour\tNumber of Bacteria')
+print(f'0 \t    {hour0}')
+print(f'5 \t    {hour5}')
+print(f'0 \t    {hour10}')
+print(f'5 \t    {hour15}')
